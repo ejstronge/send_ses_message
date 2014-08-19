89824cc41085f3e9ecfc6ee58f1cc2bfbda65927
Edward J. Stronge <ejstronge@gmail.com>
2014-08-19 12:34:40 -0400

    Move module functions to a package
    To facilitate distributing send_ses_message, this commit makes
    send_ses_message a subpackage of this directory and separates
    the callable send_smtp_ses_email from the rest of the module.

--- ses_smtp_mailer.py
+++ ses_smtp_mailer.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """
    send_ses_email.py
@@ -9,52 +10,14 @@
    (c) 2014, Edward J. Stronge Available under the MIT License - see LICENSE
    for details.
 """
-
-import sys
-
-PYTHON2 = sys.version_info.major == 2
-
 import argparse
-
-if PYTHON2:
-    import ConfigParser
-    configparser = ConfigParser
-else:
-    import configparser
-
 from email.mime.text import MIMEText
 import smtplib
+import sys
 import time
 
-
-def get_server_reference(
-        username, smtp_password, aws_smtp_endpoint, aws_smtp_port, **kwargs):
-    """
-       Returns smtplib.smtp reference using the specified parameters.
-       Call sendmail on the returned reference to send emails.
-
-       See the example configuration file for explanations on where to
-       find these parameters.
-    """
-    smtp_con = smtplib.SMTP(
-        host=aws_smtp_endpoint, port=aws_smtp_port, timeout=10)
-    smtp_con.starttls()
-    smtp_con.ehlo()
-    smtp_con.login(username, smtp_password)
-    # Will need to re-obtain this reference occasionally as server
-    # connections aren't maintained long-term
-    return smtp_con
-
-
-def get_smtp_parameters(config_file_sequence):
-    """
-       Read the configuration files in config_file_sequence
-       and return a dictionary of the parameter specified
-       in their 'default' section.
-    """
-    config_parser = configparser.SafeConfigParser()
-    config_parser.read(config_file_sequence)
-    return {i[0].lower(): i[1] for i in config_parser.items('default')}
+from send_ses_message.send_smtp_ses_email import (get_smtp_parameters, 
+    get_server_reference)
 
 
 def main():
